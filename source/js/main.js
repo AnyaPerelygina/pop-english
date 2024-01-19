cc9e1b98ed87717558f19ee1ee10c58be9ab7cd5
Anna <mynameisannanas@gmail.com>
2024-01-19 23:02:44 +0100

    feat: добавляет простой кастомный курсор

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
+import {SimpleCursor} from './utils/simple-cursor';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {addToggleMenu} from './modules/header/toggle';
@@ -23,6 +24,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const simpleCursor = new SimpleCursor();
+    simpleCursor.init();
     const stickyHeader = new StickyHeader();
     stickyHeader.init();
     initModals();
